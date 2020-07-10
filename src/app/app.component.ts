3ce81adb2f4791ef37aaa04deb491614c3b9c7d7
programmer <avengers@dillards.com>
2020-07-10 13:19:42 -0500

    named regions better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,63 +8,65 @@ import {animate, group, query, state, style, transition, trigger} from '@angular
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('sliding', [
-      // //#region true/false
-      // // TODO: working on the sliding
-      // transition('available => future', [
-      //   // query(':enter, :leave', style({position: 'fixed'}), {optional: true}),
-      //   group([
-      //     query(':leave', [
-      //       style({transform: 'translateX(0)'}),
-      //       animate('2s', style({transform: 'translateX(-200px)'}))], {optional: true}
-      //     ),
-      //     query(':enter', [
-      //       style({transform: 'translateX(200px)'}),
-      //       animate('2s', style({transform: 'translateX(0)'}))], {optional: true}
-      //     ),
-      //   ])
-      // ]),
-      // transition('future => available', [
-      //   // query(':enter, :leave', style({position: 'fixed'}), {optional: true}),
-      //   group([
-      //     query(':leave', [
-      //       style({transform: 'translateX(0)'}),
-      //       animate('2s', style({transform: 'translateX(200px)'}))], {optional: true}
-      //     ),
-      //     query(':enter', [
-      //       style({transform: 'translateX(-200px)'}),
-      //       animate('2s', style({transform: 'translateX(0)'}))], {optional: true}
-      //     ),
-      //   ])
-      // ])
-      // //#endregion
-      state('in', style({
-        width: 150,
-        transform: 'translateX(0)', opacity: 1
-      })),
+      //#region transitions query
+      // TODO: working on the sliding
       transition('available => future', [
-        style({ width: 150, transform: 'translateX(100px)', opacity: 1 }),
+        // query(':enter, :leave', style({position: 'fixed'}), {optional: true}),
         group([
-          animate('0.3s 0.1s ease', style({
-            transform: 'translateX(0)',
-            width: 150,
-          })),
-          animate('0.3s ease', style({
-            opacity: 1,
-          }))
+          query(':leave', [
+            style({transform: 'translateX(0)'}),
+            animate('2s', style({transform: 'translateX(-200px)'}))], {optional: true}
+          ),
+          query(':enter', [
+            style({transform: 'translateX(200px)'}),
+            animate('2s', style({transform: 'translateX(0)'}))], {optional: true}
+          ),
         ])
       ]),
       transition('future => available', [
-        style({ width: 150, transform: 'translateX(-100px)', opacity: 1 }),
+        // query(':enter, :leave', style({position: 'fixed'}), {optional: true}),
         group([
-          animate('0.3s ease', style({
-            transform: 'translateX(0px)',
-            width: 150,
-          })),
-          animate('0.3s 0.2s ease', style({
-            opacity: 1
-          }))
+          query(':leave', [
+            style({transform: 'translateX(0)'}),
+            animate('2s', style({transform: 'translateX(200px)'}))], {optional: true}
+          ),
+          query(':enter', [
+            style({transform: 'translateX(-200px)'}),
+            animate('2s', style({transform: 'translateX(0)'}))], {optional: true}
+          ),
         ])
       ])
+      //#endregion
+      //#region usingState
+      // state('in', style({
+      //   width: 150,
+      //   transform: 'translateX(0)', opacity: 1
+      // })),
+      // transition('available => future', [
+      //   style({ width: 150, transform: 'translateX(100px)', opacity: 1 }),
+      //   group([
+      //     animate('0.3s 0.1s ease', style({
+      //       transform: 'translateX(0)',
+      //       width: 150,
+      //     })),
+      //     animate('0.3s ease', style({
+      //       opacity: 1,
+      //     }))
+      //   ])
+      // ]),
+      // transition('future => available', [
+      //   style({ width: 150, transform: 'translateX(-100px)', opacity: 1 }),
+      //   group([
+      //     animate('0.3s ease', style({
+      //       transform: 'translateX(0px)',
+      //       width: 150,
+      //     })),
+      //     animate('0.3s 0.2s ease', style({
+      //       opacity: 1
+      //     }))
+      //   ])
+      // ])
+      //#endregion
     ])
   ]
 })
